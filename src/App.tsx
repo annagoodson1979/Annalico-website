bed5b35da36e4164e7af07397b3c8a8c79692831
annagoodson1979 <annagoodson1979@gmail.com>
2026-02-16 22:15:33 -0600

    Fix App component return statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // src/App.tsx}
 
-export default function App() {}
+export default function App() {
 function App() {}
-return ()
+return (
 <div>
   {/*<h1>Hello from React!</h1>*/}
 <p>If you see this, the build is working.</p>
