6af33d8ef3e16fbd16b66a4cb5f1292111e91509
annagoodson1979 <annagoodson1979@gmail.com>
2026-02-16 21:02:36 -0600

    Initialize main.tsx with React app rendering

---
+++ main.tsx
@@ -0,0 +1,16 @@
+// src/main.tsx
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// This is the root element in your index.html
+const rootElement = document.getElementById('root');
+
+// If the element exists, render the App
+if (rootElement) {
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
+} else {
+console.error('Could not find the #root element in index.html!');
+}
