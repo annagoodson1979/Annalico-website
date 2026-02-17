e40582fb6215d5ea5a2aae993006bd06360f143d
annagoodson1979 <annagoodson1979@gmail.com>
2026-02-16 22:05:04 -0600

    Comment out the 'Hello from React!' header
    Comment out the header in the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-// src/App.tsx} 
+// src/App.tsx
 
 export default function App() {
 function App() {
 return (
 <div>
-<h1>Hello from React!</h1>
+  {/*<h1>Hello from React!</h1>*/}
 <p>If you see this, the build is working.</p>
 </div>
 );
