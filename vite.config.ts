86d8d779bd66a16c9b2831c3d29832877af8d265
annagoodson1979 <annagoodson1979@gmail.com>
2026-04-18 22:31:59 -0500

    Deploy latest site

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // Use root for custom domain (CNAME present)
-})
